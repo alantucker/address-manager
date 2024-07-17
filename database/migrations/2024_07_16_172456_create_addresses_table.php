654f8f279d353cb5123ac1789d4f1c0cb47c0d85
Alan Tucker <alan.tucker@gmail.com>
2024-07-17 23:15:31 +0100

    Create 'addresses' database migration
    A new migration for creating the 'addresses' table has been added. This table includes fields for company name, two address lines, city, county, postcode, country, longitude, and latitude.

---
+++ database/migrations/2024_07_16_172456_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('company_name', 60);
+            $table->string('address_1');
+            $table->string('address_2')->nullable();
+            $table->string('city', 30)->nullable();
+            $table->string('county', 30);
+            $table->string('postcode', 8);
+            $table->string('country', 65);
+            $table->decimal('longitude', 11,8)->nullable();
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
