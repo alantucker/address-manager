c52a973833dd95ac972df18d66992567fe154b1a
Alan Tucker <alan.tucker@gmail.com>
2024-07-17 23:14:43 +0100

    Add new routes for AddressController
    Implemented new routing paths in the 'web.php' for the AddressController functions. This includes search operations both by postcode and by id with appropriate routes for index, show, and store actions.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AddressController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/bypostcode', [AddressController::class, 'byPostcodeIndex'])->name('bypostcode.index');
+Route::post('/bypostcode', [AddressController::class, 'byPostcodeShow'])->name('bypostcode.show');
+Route::post('/bypostcode/store', [AddressController::class, 'byPostcodeStore'])->name('bypostcode.store');
+
+Route::get('/byid', [AddressController::class, 'byIdIndex'])->name('byid.index');
+Route::post('/byid', [AddressController::class, 'byIdShow'])->name('byid.show');
