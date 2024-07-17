f5c73751b665e4b18d1979299119e9e6e9628a6b
Alan Tucker <alan.tucker@gmail.com>
2024-07-17 23:17:24 +0100

    Add API routes for address lookup
    This commit introduces API routes for the address lookup functionality. It includes endpoints to fetch addresses by postcode or ID. Additionally, it provides an endpoint to find specific addresses using an ID, and an authenticated endpoint to return user details based on a request.

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Api\V1\AddressController;
+use App\Http\Resources\AddressResource;
+use App\Models\Address;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/v1/lookup/{postcode}', [AddressController::class, 'index']);
+Route::get('/v1/lookup/show/{id}', [AddressController::class, 'show']);
+
+Route::get('/v1/lookup/find/{id}', function (string $id) {
+    return new AddressResource(Address::findOrFail($id));
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
