81a8183b08efae5a8b0267a09a12750f303bb680
Alan Tucker <alan.tucker@gmail.com>
2024-07-18 16:37:51 +0100

    Add 'store' method to AddressController
    A 'store' method has been added to the AddressController class. This method fetches a new address using an external API and stores it in the database if it doesn't already exist. In case of any errors (like the API request failing, no results, or address already existing in the database), appropriate json responses with status and message are returned.

--- app/Http/Controllers/Api/V1/AddressController.php
+++ app/Http/Controllers/Api/V1/AddressController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\SearchAddressRequest;
 use App\Models\Address;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class AddressController extends Controller
 {
@@ -37,6 +38,63 @@ class AddressController extends Controller
             'data' => $response->suggestions
         ], 200);
     }
+
+
+    /**
+     * Store a new address
+     */
+    public function store(Request $request)
+    {
+        $response = Http::get('api.getAddress.io/get/' . $request->id . '?api-key=' . config('address.address_api_key'));
+
+        if (!$response->successful()) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Something went wrong'
+            ], 400);
+        }
+
+        if ($response == []) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'No search results'
+            ], 404);
+        }
+
+        $response = json_decode($response->body());
+
+        $addressCheck = Address::where([
+            "address_1" => $response->line_1,
+            "postcode" => $response->postcode
+        ])->get();
+
+        if ($addressCheck->isNotEmpty()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Address already exists'
+            ], 400);
+        }
+
+        $new_address = Address::create([
+            'id' => Str::uuid(),
+            'company_name' => $response->residential === false ? $response->line_1 : "",
+            'address_1' => $response->residential === false ? $response->line_2 : $response->line_1,
+            'address_2' => $response->residential === false ? "" : $response->line_2,
+            'city' => $response->town_or_city,
+            'county' => $response->county,
+            'postcode' => $response->postcode,
+            'country' => $response->country,
+            'longitude' => $response->longitude,
+            'latitude' => $response->latitude,
+        ]);
+
+        return response()->json([
+            'data' => $new_address
+        ], 200);
+
+    }
+
+
 
     /**
      * Display the specified resource.
